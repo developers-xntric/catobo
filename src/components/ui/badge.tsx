aacd0a3c8f48b285f7a0d565c3af3adb1279ca8c
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2026-06-04 13:15:54 +0500

    press lsit page compelted

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,12 +1,13 @@
-const Badge = ({ text, variant = "white" }: { text: string; variant?: "black" | "white" }) => {
+const Badge = ({ text, variant = "white", className = "" }: { text: string; variant?: "black" | "white"; className?: string }) => {
     return (
-        <div className="flex items-center gap-2 mb-4 border px-3 py-2 rounded-full w-fit border-[rgba(0,0,0,0.10)] mx-auto">
+        <div className={`flex items-center gap-2 mb-4 border px-3 py-2 rounded-full w-fit border-[rgba(0,0,0,0.10)] ${className}`}>
             <span className="w-1.5 h-1.5 rounded-full bg-[#168DCA]" />
             <span className={`md:text-sm text-xs  ${variant == "black" ? "text-black" : "text-white"} `}>
                 {text}
             </span>
         </div>
     )
-} 
+}
+
 
 export default Badge
